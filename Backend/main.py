871b3957da48ef40955e18dde072df4a0838f1c3
destripador2000 <davidhumbertop503@gmail.com>
2026-03-25 18:35:33 -0600

    Feat: Agregando router al main para el testeo

--- Backend/main.py
+++ Backend/main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 from core.config import settings
 from routers.rt_product import router as routerProduct
 from routers.rt_supplier import router as routerSupplier
+from routers.rt_inventary import router as routerInventary
 from models import *
 
 app = FastAPI(title=settings.app_name)
 
 app.include_router(routerProduct, prefix="/router/rt_product", tags=["product"])
 app.include_router(routerSupplier, prefix="/router/rt_supplier", tags=["supplier"])
+app.include_router(routerInventary, prefix="/router/rt_inventary", tags=["inventary"])
 
 app.get("/")
 async def root():
