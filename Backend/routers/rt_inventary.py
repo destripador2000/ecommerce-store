90d3b487fa99cfdc431a876f209192f3389ef53e
destripador2000 <davidhumbertop503@gmail.com>
2026-03-26 20:34:30 -0600

    Feat: creación API para actualizar inventario aún desarrollo

--- Backend/routers/rt_inventary.py
+++ Backend/routers/rt_inventary.py
@@ -1,8 +1,7 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from sqlmodel import col
-from typing import List
 
 from database.db import get_db
 from models.md_Inventary import Inventary as db_Inventary
@@ -13,11 +12,11 @@ router = APIRouter()
 
 # * API  para filtros de inventario
 @router.get("/")
-async def read_product(skip: int = 0,
+async def read_inventary(skip: int = 0,
                         limit: int = 10,
                         lowStock: bool = False,
-                        in_stock: bool | None = None,
-                        product_id: int | None = None,
+                        inStock: bool | None = None,
+                        productId: int | None = None,
                         conex: AsyncSession = Depends(get_db)):
 
     try:
@@ -26,21 +25,48 @@ async def read_product(skip: int = 0,
         if lowStock is True:
             stmt = stmt.where(col(db_Inventary.actualStock <= db_Inventary.minimStock))
         
-        if product_id is not None:
-            stmt = stmt.where(col(db_Inventary.productID) == product_id)
+        if productId is not None:
+            stmt = stmt.where(col(db_Inventary.productID) == productId)
         
-        if in_stock is True:
+        if inStock is True:
             stmt = stmt.where(col(db_Inventary.actualStock) > 0)
-        elif in_stock is False:
+        elif inStock is False:
             stmt = stmt.where(col(db_Inventary.actualStock) == 0)
             
         stmt = stmt.offset(skip).limit(limit)
         result = await conex.execute(stmt)
         inventaryRead = result.scalars().all()
-
+    
     except Exception as e:
         await conex.rollback()
         logger.error(f"ERROR: {e}")
         raise HTTPException(status_code=500, detail= "Error con el servidor")
     
     return inventaryRead
+
+# * API para modificar inventario (stocks)
+@router.patch("/")
+async def update_intenvary(idInventary: int,
+                            mdInventary: Inventary,
+                            conex: AsyncSession = Depends(get_db)):
+    
+    try:
+        if idInventary is not None:
+            stmt = select(col(db_Inventary.id) == idInventary)
+        
+        result = await conex.execute(stmt)
+        inventary = result.scalars().first()
+        mdInventary.model_dump()
+
+        for llave, valor in mdInventary:
+            setattr(db_Inventary.id, llave, valor)
+
+        await conex.commit()
+        await conex.refresh()
+
+    except Exception as e:
+        await  conex.rollback()
+        logger.error(f"ERROR: {e}")
+        raise HTTPException(status_code=404, detail="Inventario inexistente")
+    
+    return inventary
