578c9b9fede7e319ed646cac19c3626390b5bd6e
destripador2000 <davidhumbertop503@gmail.com>
2026-03-28 18:59:28 -0600

    Feat: Creación del esquema para actualizar productos

--- Backend/schemas/sc_Product.py
+++ Backend/schemas/sc_Product.py
@@ -10,3 +10,9 @@ class Product(BaseModel):
 
     class Config:
         from_attributes = True
+
+class UpdateProduct(BaseModel):
+    name: str | None = None
+    description: str | None = None
+    basePrice: float | None = None
+    urlImage: str | None = None
