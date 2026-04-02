6a46f57c446ae5317b1b81a0f6107939dce564c1
destripador2000 <davidhumbertop503@gmail.com>
2026-04-02 17:32:05 -0600

    Feat: Agregando CORS al main.py para comunicación con el frontend

--- Backend/main.py
+++ Backend/main.py
@@ -4,8 +4,23 @@ from routers.rt_product import router as routerProduct
 from routers.rt_supplier import router as routerSupplier
 from routers.rt_inventary import router as routerInventary
 from models import *
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title=settings.app_name)
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://ecommerce-store-seven-murex.vercel.app/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(routerProduct, prefix="/router/rt_product", tags=["product"])
 app.include_router(routerSupplier, prefix="/router/rt_supplier", tags=["supplier"])
